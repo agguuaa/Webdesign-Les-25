66c58bf714109327418324bbde51ceec9c85765c
agguuaa <100287322+agguuaa@users.noreply.github.com>
2023-08-17 21:01:41 +0200

    website created

---
+++ script.js
@@ -0,0 +1,5 @@
+const gamepadButton = document.querySelector("#button button");
+
+gamepadButton.addEventListener("click", function () {
+  alert("Hey! Kind reminder: you are doing a great job!");
+});
